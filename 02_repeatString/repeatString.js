907fff266006ccaebc8b520e6b8f2013a4f05d47
David Lovcov <david_lovcov@MacBook-Pro-von-David.fritz.box>
2024-10-30 14:45:15 +0100

    First init

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repeatAmount) {
+    let fullPhrase = '';
+    if (repeatAmount < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < repeatAmount; i++) {
+        fullPhrase += phrase;
+    }
+    return fullPhrase;
 };
 
 // Do not edit below this line
