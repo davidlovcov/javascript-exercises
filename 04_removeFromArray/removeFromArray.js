2492e529b87db7a12483f3b05628e1063d88fafd
David Lovcov <david_lovcov@MacBook-Pro-von-David.fritz.box>
2024-10-30 16:36:14 +0100

    Done task 1 - 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToRemoveFrom) {
+    const args = Array.from(arguments);
+    args.shift();
+    return arrayToRemoveFrom = arrayToRemoveFrom.filter( (element) => !args.includes( element ));
 };
 
 // Do not edit below this line
